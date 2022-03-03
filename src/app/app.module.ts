b9cf3b82cdd139c28ed37b3f4e2eb1f9db211fe6
Nico <nicolasarley349@gmail.com>
2022-03-02 20:04:57 -0500

    add new section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { FeaturesComponent } from './features/features.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PricingComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    FeaturesComponent
   ],
   imports: [
     BrowserModule,
